18f5985fc7249cafab1a177f483d5c3860f6750c
IanRafael4 <delapenasalazarianrafael@gmail.com>
2025-03-06 22:43:21 -0600

    Actualizar el archivo con el menu

--- G4-Cal.c
+++ G4-Cal.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
-int main(){
- printf("Hola\n");
+#include <stdlib.h>
 
+void suma();
+void resta();
+void multiplicacion();
+void Division();
+
+int main() {
+    int opcion;
+    do {
+        printf("\nCalculadora Basica\n");
+        printf("1. Suma\n");
+        printf("2. Resta\n");
+        printf("3. Multiplicacion\n");
+        printf("4. Division\n");
+        printf("5. Salir\n");
+        printf("Seleccione una opcion: ");
+        scanf("%d", &opcion);
+
+        switch(opcion) {
+            case 1: suma(); break;
+            case 2: resta(); break;
+            case 3: multiplicacion(); break;
+            case 4: division(); break;
+            case 5: printf("Saliendo...\n"); break;
+            default: printf("Opcion no valida. Intente de nuevo.\n");
+        }
+    } while(opcion != 5);
+
+    return 0;
 }
+
+
